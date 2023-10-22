5c201e6e338921b7b385d1ddeec3600dde5b9e0d
jacekroszkowiak <jacekroszkowiakdev@gmail.com>
2023-10-23 00:05:19 +0200

    styling contact form finished

--- week1/day2/Portfolio/tailwind.config.js
+++ week1/day2/Portfolio/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            boxShadow: {
+                "3xl": "0px 6px 25px 3px rgba(0,0,0,0.3)",
+            },
+        },
     },
     plugins: [],
 };
